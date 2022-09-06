4896ae857caa709c0f11bf90103e6a1fbe94929c
ksranderson <randerson7000@protonmail.com>
2022-09-06 15:57:15 -0400

    Week 2 assignment commit RA

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Peer assignment week 3
+#Write two R functions, one to cache matrix, one to solve the matrix's inverse
+#Reuben Anderson
 
-## Write a short comment describing this function
+## Caches matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+  m <- NULL				#matrix cache variable, set to NULL
+  set <- function(y) {	#set matrix function with input y variable 
+    x <<- y			#set x to global y
+    m <<- NULL		#set m to NULL
+  }
+  get <- function() x		#get matrix data, NOT the matrix ex. c(1,2,3,4,5,6,7,8,9) for a square matrix 
+  setmatrix <- function(matrix) m <<- matrix #set m to the matrix
+  getmatrix <- function() m	#get matrix from m where it is cached
+  list(set = set, get = get,			#list to return for object makeCacheMatrix
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+## Solves matrix's inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getmatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(matrix(data,nrow= sqrt(length(data)),ncol= sqrt(length(data)), byrow=TRUE), tol = 1e-20)	#here is where it actually calculates inverse matrix
+  x$setmatrix(m)
+  m
 }
